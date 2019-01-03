5cbdcaabc1a2b35f5070248e8172fc15ba9bfa07
DevOps Tinman <devops.tinman@gmail.com>
2019-01-03 07:44:25 -0600

    Initial Commit

---
+++ app-dev-profile/app-dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-dev-profile
+//
+//  Created by Allbee, Eamon on 1/3/19.
+//  Copyright © 2019 Allbee, Eamon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
